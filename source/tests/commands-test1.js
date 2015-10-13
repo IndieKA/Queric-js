f29d7e28ad48f51c2bab4159cba0e48d149b02e4
KingCodeFish <kingcodefish@gmail.com>
2015-10-12 21:45:50 -0400

    Update commands-test1.js

--- source/tests/commands-test1.js
+++ source/tests/commands-test1.js
@@ -1,7 +1,7 @@
 // Interactive "create."
 // Same thing with "graph", but different.
 
-create.chart(x, y, rowNum, rowSections){
-  chartName: ""
-  addTo(chart): "";
+create.chart(x, y, rowNum, rowSections) {
+    chartName: ""
+    addTo(chart): "";
 }
